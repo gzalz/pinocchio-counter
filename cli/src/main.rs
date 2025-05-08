9c7d8bb46a25804b200eea49c4d32747b4a00014
gzalz <ericdaniel.gonzalez+github@gmail.com>
2025-05-07 22:45:03 -0600

    working no impl

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -11,31 +11,32 @@ use solana_sdk::instruction::AccountMeta;
  use std::str::FromStr;
 
 fn main() {
-    // RPC and payer setup
-    let rpc_url = "http://api.devnet.solana.com";
+    let rpc_url = "http://localhost:8899";
     let client = RpcClient::new(rpc_url.to_string());
-    let payer = read_keypair_file("/Users/eric/.config/solana/id.json").unwrap(); // or read from file
+    let payer = read_keypair_file("/Users/eric/.config/solana/id.json").unwrap();
 
-    // The program to invoke
-    let program_id = Pubkey::from_str("CS6YTetM9Z1sot4nPyF46qGQAfpmFtNmTM9vEcUrtcDB").unwrap();
-    let dest_pubkey = Pubkey::from_str("4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU").unwrap();
+    let program_id = Pubkey::from_str("H7NQGd5ZDZtHJNmCpgyi6b3kuoJpS8mvQCrVrg9yRt9V").unwrap();
+    let (pda_pubkey, bump) = Pubkey::find_program_address(
+        &[b"counter".as_ref()],
+        &program_id,
+    );
+
     let system_program = Pubkey::from_str("11111111111111111111111111111111").unwrap();
+    let sysvar_rent = Pubkey::from_str("SysvarRent111111111111111111111111111111111").unwrap();
 
-    // Optional: Accounts to include (can be empty or just the program itself)
     let accounts = vec![
-        AccountMeta::new(payer.pubkey(), true), // writable + signer
-        AccountMeta::new(dest_pubkey, false),
+        AccountMeta::new(payer.pubkey(), true),
+        AccountMeta::new(pda_pubkey, false),
         AccountMeta::new(system_program, false),
+        AccountMeta::new(sysvar_rent, false),
     ];
 
-    // Instruction with no data
     let instruction = Instruction {
         program_id,
         accounts,
-        data: vec![42, 0, 0, 0, 0, 0, 0, 0, 42, 0, 0, 0], // empty data
+        data: vec![2, bump],
     };
 
-    // Build and send the transaction
     let recent_blockhash = client.get_latest_blockhash().unwrap();
     let message = Message::new(&[instruction], Some(&payer.pubkey()));
     let tx = Transaction::new(&[&payer], message, recent_blockhash);
--- src/lib.rs
+++ src/lib.rs
@@ -4,31 +4,76 @@ use pinocchio::{
     default_allocator, default_panic_handler, entrypoint,
     entrypoint::lazy::MaybeAccount,
     entrypoint::InstructionContext,
+    instruction::{Seed, Signer},
     lazy_program_entrypoint, msg,
     program_error::{ProgramError, INVALID_ACCOUNT_DATA},
+    pubkey,
     pubkey::Pubkey,
+    sysvars::{rent::Rent, Sysvar},
     ProgramResult,
 };
 
-lazy_program_entrypoint!(process_instruction);
-default_allocator!();
-default_panic_handler!();
+entrypoint!(process_instruction);
 
-#[repr(C)]
-#[derive(Debug)]
-pub struct ZeroCopy {
-    pub field1: u64,
-    pub field2: u32,
+const INITIALIZE: u8 = 1;
+const INCREMENT: u8 = 2;
+
+struct Counter {
+    pub value: u64,
 }
 
-pub fn process_instruction(mut context: InstructionContext) -> ProgramResult {
-    if context.available() <= 2 {
-        return Err(ProgramError::InvalidAccountData);
+impl Counter {
+    pub fn load_mut(account_data: &[u8]) -> Self {
+        Self {
+            value: account_data[0..8]
+                .try_into()
+                .map(u64::from_le_bytes)
+                .map_err(|_| INVALID_ACCOUNT_DATA).unwrap(),
+        }
     }
-    let _ = context.next_account();
-    let zc_account_info: AccountInfo = context.next_account().unwrap().assume_account();
-    let zc_data = zc_account_info.try_borrow_data().unwrap();
-    let ptr = zc_data.as_ptr() as *const ZeroCopy;
-    let zc_struct = unsafe { &*ptr };
+
+    pub fn increment(&mut self) {
+        self.value = self.value.wrapping_add(1);
+    }
+}
+
+pub fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    let ix_discr = instruction_data[0];
+    if ix_discr == INITIALIZE {
+        let payer_info: &AccountInfo = &accounts[0];
+        let pda_info: &AccountInfo = &accounts[1];
+
+        let bump = [instruction_data[1]];
+
+        let seed = [Seed::from(b"counter"), Seed::from(&bump)];
+        let seeds = Signer::from(&seed);
+        let size = 8;
+
+        pinocchio_system::instructions::CreateAccount {
+            from: &payer_info,
+            to: &pda_info,
+            lamports: Rent::get()?.minimum_balance(size),
+            space: size as u64,
+            owner: &program_id,
+        }
+        .invoke_signed(&[seeds.clone()])?;
+
+        pda_info.realloc(size, false)?;
+    } else if ix_discr == INCREMENT {
+        let pda_info: &AccountInfo = &accounts[1];
+        let mut data = pda_info.try_borrow_mut_data()?;
+        let counter_value = data[0..8]
+            .try_into()
+            .map(u64::from_le_bytes)
+            .map_err(|_| INVALID_ACCOUNT_DATA)?;
+        let new_counter_value = counter_value.wrapping_add(1);
+        data[0..8].copy_from_slice(&new_counter_value.to_le_bytes());
+    } else {
+        return Err(ProgramError::InvalidInstructionData);
+    }
     Ok(())
 }
